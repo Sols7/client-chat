97df3c30c57cd56db1f4ed28533ddd0122eab310
Sols7 <serg-lex@yandex.ru>
2022-01-02 09:54:19 +0300

    changed package services, client lesson 3

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,28 +1,13 @@
 package ru.itsjava;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import ru.itsjava.services.ClientService;
+import ru.itsjava.services.ClientServiceImpl;
 
 public class Main {
-    public static final int PORT = 8081;
-    public static final String HOST = "localhost";
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(HOST, PORT);
-
-        if (socket.isConnected()){
-            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
-            serverWriter.println("Hi from client!");
-            serverWriter.flush();
-
-            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while (true){
-                System.out.println(serverReader.readLine());
-            }
-        }
+    public static void main(String[] args) {
+        ClientService clientService = new ClientServiceImpl();
+        clientService.start();
 
     }
 }
---
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -0,0 +1,35 @@
+package ru.itsjava.services;
+
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientServiceImpl implements ClientService{
+    public static final int PORT = 8081;
+    public static final String HOST = "localhost";
+
+    @SneakyThrows
+    @Override
+    public void start() {
+        Socket socket = new Socket(HOST, PORT);
+
+        if (socket.isConnected()){
+            new Thread(new SocketRunnable(socket)).start();
+
+            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
+
+            MessageInputService messageInputService = new MessageInputServiceImpl(System.in);
+
+            System.out.println("Введите сообщение");
+            String consoleMessage = messageInputService.getMessage();
+
+            serverWriter.println(consoleMessage);
+            serverWriter.flush();
+
+
+        }
+    }
+}
