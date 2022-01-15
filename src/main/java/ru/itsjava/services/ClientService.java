9acc84e86e8751c29a8bcc5423a21597555d4240
Sols7 <serg-lex@yandex.ru>
2022-01-15 20:44:33 +0300

    changed client-chat lesson 5

--- src/main/java/ru/itsjava/services/ClientService.java
+++ src/main/java/ru/itsjava/services/ClientService.java
@@ -3,4 +3,6 @@ package ru.itsjava.services;
 public interface ClientService {
 
     void start();
+
+    void printMenu();
 }
--- src/main/java/ru/itsjava/services/ClientServiceImpl.java
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -2,8 +2,6 @@ package ru.itsjava.services;
 
 import lombok.SneakyThrows;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -18,22 +16,36 @@ public class ClientServiceImpl implements ClientService {
 
         if (socket.isConnected()) {
             new Thread(new SocketRunnable(socket)).start();
-
             PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
-
             MessageInputService messageInputService = new MessageInputServiceImpl(System.in);
 
-            System.out.println("Введите свой логин:");
-            String login = messageInputService.getMessage();
+            printMenu();
+            System.out.println("Введите номер меню");
+            int numMenu = Integer.parseInt(messageInputService.getMessage());
 
-            System.out.println("Введите свой пароль:");
-            String password = messageInputService.getMessage();
+            if (numMenu == 1) {
+                System.out.println("Авторизация");
+                System.out.println("Введите свой логин:");
+                String login = messageInputService.getMessage();
 
-            //!autho!login:password
-            serverWriter.println("!autho!" + login + ":" + password);
-            serverWriter.flush();
+                System.out.println("Введите свой пароль:");
+                String password = messageInputService.getMessage();
 
-//            while (!messageInputService.getMessage().equalsIgnoreCase("exit")) {
+                //!autho!login:password
+                serverWriter.println("!autho!" + login + ":" + password);
+                serverWriter.flush();
+            } else if (numMenu == 2) {
+                System.out.println("Регистрация");
+                System.out.println("Введите логин:");
+                String login = messageInputService.getMessage();
+
+                System.out.println("Введите пароль:");
+                String password = messageInputService.getMessage();
+                //!reg!login:password
+                serverWriter.println("!reg!" + login + ":" + password);
+                serverWriter.flush();
+            }
+
             while (true) {
                 //System.out.println("Введите сообщение");
                 String consoleMessage = messageInputService.getMessage();
@@ -42,10 +54,14 @@ public class ClientServiceImpl implements ClientService {
                 serverWriter.flush();
 
                 if (consoleMessage.equalsIgnoreCase("exit")) {
-//                    break;
                     System.exit(0);
                 }
             }
         }
+    }
+
+    @Override
+    public void printMenu() {
+        System.out.println("1 - Авторизация; 2 - Регистрация");
     }
 }
